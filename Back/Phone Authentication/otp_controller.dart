d015fc330605cd10138707255934a3802fbc4c2a
leasalameh <121007958+leasalameh@users.noreply.github.com>
2024-02-25 18:58:41 +0200

    Delete Back/Phone Authentication directory

--- Back/Phone Authentication/otp_controller.dart
+++
@@ -1,8 +0,0 @@
-class OTPController extends GetxController {
-  static OTPController get instance => Get.find();
-
-  void verifyOTP(String otp) async {
-    var isVerified = await AuthenticationRepository.instance.verifyOTP(otp);
-    isVerified ? Get.offAll(const Dashboard()) : Get.back();
-  }
-}
