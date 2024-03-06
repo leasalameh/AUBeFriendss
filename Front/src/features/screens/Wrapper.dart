f98b86226ac4558a0e9ff3c8d3835abd3ad98216
rba17 <145935407+rba17@users.noreply.github.com>
2024-03-06 10:30:11 +0200

    Add files via upload

---
+++ Front/src/features/screens/Wrapper.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:project_app/src/features/screens/home/home_screen.dart';
+import 'package:project_app/src/features/screens/welcome/welcome_screen.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final currentUser = Provider.of<User?>(context);
+    if (currentUser == null)
+      return WelcomeScreen();
+    else
+      return Home();
+  }
+}
