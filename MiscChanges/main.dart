76d26343dbcb6a5524bb2884777d906eae3ab2c4
rba17 <145935407+rba17@users.noreply.github.com>
2024-03-06 10:27:08 +0200

    Add files via upload

--- MiscChanges/main.dart
+++ MiscChanges/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project_app/backend/auth.dart';
 import 'package:project_app/src/constants/text_strings.dart';
+import 'package:project_app/src/features/screens/Wrapper.dart';
 import 'package:project_app/src/features/screens/forgetPassword/forget_password_bottom_sheet.dart';
 import 'package:project_app/src/features/screens/forgetPassword/forget_password_mail/forget_password_mail_screen.dart';
 import 'package:project_app/src/features/screens/forgetPassword/otp/otp_screen.dart';
@@ -9,23 +12,39 @@ import 'package:project_app/src/features/screens/profile/update_profile_screen.d
 import 'package:project_app/src/features/screens/signup/signup_screen.dart';
 import 'package:project_app/src/features/screens/welcome/welcome_screen.dart';
 import 'package:project_app/src/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(App());
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure plugin services are initialized
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCBIayceBjqiqi3yBrtt3D3lFvV_wXdtV0",
+          appId: "1:968237937887:android:a6d36537afc19126ef256a",
+          messagingSenderId: "968237937887",
+          projectId: "aubefriends2024"));
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      // defaultTransition: Transition.leftToRightWithFade,
-      // transitionDuration: const Duration(milliseconds: 500),
-      home: WelcomeScreen(),
+    return StreamProvider<User?>.value(
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(
+        theme: TAppTheme.lightTheme,
+        darkTheme: TAppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        // defaultTransition: Transition.leftToRightWithFade,
+        // transitionDuration: const Duration(milliseconds: 500),
+        home: const Wrapper(),
+      ),
     );
   }
 }
