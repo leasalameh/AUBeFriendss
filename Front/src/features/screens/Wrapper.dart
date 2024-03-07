979aae077478aa83b8cf362f92f3878170a9af15
leasalameh <121007958+leasalameh@users.noreply.github.com>
2024-03-07 09:47:29 +0200

    Update Wrapper.dart

--- Front/src/features/screens/Wrapper.dart
+++ Front/src/features/screens/Wrapper.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:project_app/src/features/screens/home/home_screen.dart';
+import 'package:project_app/src/features/screens/home/homeScreen.dart';
 import 'package:project_app/src/features/screens/welcome/welcome_screen.dart';
 import 'package:provider/provider.dart';
 
